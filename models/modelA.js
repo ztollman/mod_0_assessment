38416df26a48137de0be707f00f5909ebd6d50da
Zach <ztollman@gmail.comm>
2021-04-22 21:02:16 -0600

    Airport complete

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var Airport = {
+  name = 'dia',
+  state = 'colorado',
+  elevation = 5430,
+  openYear = 1995,
+  isClean = false,
+  isNice = True,
+  lot = ['cars','bus','wagon','planes'],
+  interior = ['bathrooms','elevators','escalators','gates'],
+}
