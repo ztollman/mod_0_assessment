8beeb31c21153e665b84f31cb52730a897011dab
Zach <ztollman@gmail.comm>
2021-04-22 20:58:38 -0600

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,14 @@
+var famousPerson = "Brad";
+var isActive = true
+famousPerson = "George";
+var num3 = 3
+var num9 = 9
+var sum = num3 + num9
+console.log(sum)
+var animals = new Array(3)
+console.log(animals)
+animals.shift()
+console.log(animals)
+for (var i = 0; i < 1; i++){
+  console.log(famousPerson);
+}
